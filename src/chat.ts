2f696c40faf972d3638fe7f03e39d3467ad35608
Vi-brance <vi_brance@outlook.com>
2022-12-30 16:52:24 +0800

    补充代码注释，同步各函数参数名

--- src/chat.ts
+++ src/chat.ts
@@ -2,21 +2,25 @@ import * as vscode from 'vscode';
 import * as oicq from 'oicq';
 import { Global } from './global';
 
+// 发送给页面的数据类
 interface WebviewPostData {
     command: keyof oicq.Client,
     params: any[],
     echo: string
 }
 
-const webviewMap: Map<number, vscode.WebviewPanel> = new Map;
+// 页面缓存, 按聊天类型和对象id建立与聊天页面的索引
+const webviewMap: Map<[boolean, number], vscode.WebviewPanel> = new Map;
 
 vscode.commands.registerCommand("qlite.chat", openChatView);
 
-function setHtml(uid: number, type: boolean, webview: vscode.Webview): string {
-    const preload = webview.asWebviewUri(vscode.Uri.joinPath(Global.context.extensionUri, "assets", "preload.js")).toString();
-    const css = webview.asWebviewUri(vscode.Uri.joinPath(Global.context.extensionUri, "assets", "style.css")).toString();
-    const js = webview.asWebviewUri(vscode.Uri.joinPath(Global.context.extensionUri, "assets", "app.js")).toString();
-    const path = webview.asWebviewUri(vscode.Uri.joinPath(Global.context.extensionUri, "assets")).toString();
+// 初始化页面
+function setHtml(uid: number, c2c: boolean, webview: vscode.Webview): string {
+    const assetUri = vscode.Uri.joinPath(Global.context.extensionUri, "assets");
+    const path = webview.asWebviewUri(assetUri).toString();
+    const preload = webview.asWebviewUri(vscode.Uri.joinPath(assetUri, "preload.js")).toString();
+    const css = webview.asWebviewUri(vscode.Uri.joinPath(assetUri, "style.css")).toString();
+    const js = webview.asWebviewUri(vscode.Uri.joinPath(assetUri, "app.js")).toString();
     return `<!DOCTYPE html>
     <html>
     <head>
@@ -26,41 +30,39 @@ function setHtml(uid: number, type: boolean, webview: vscode.Webview): string {
         <link rel="stylesheet" type="text/css" href="${css}" />
     </head>
     <body>
-        <env self_id="${Global.client.uin}" nickname="${Global.client.nickname}" c2c="${type ? 0 : 1}" target_id="${uid}" temp="0" path="${path}" t="${Date.now()}">
+        <env self_id="${Global.client.uin}" nickname="${Global.client.nickname}" c2c="${c2c ? 1 : 0}" target_id="${uid}" temp="0" path="${path}" t="${Date.now()}">
         <script src="${preload}"></script>
         <script src="${js}"></script>
     </body>
     </html>`;
 }
 
-// this function is called when click a friend or a group
-function openChatView(uid: number, type: boolean) {
-    // get view name
-    let label: string = type ? Global.client.gl.get(uid)?.group_name as string : Global.client.fl.get(uid)?.remark as string;
-    if (webviewMap.has(uid)) { // this view has generated before
-        return webviewMap.get(uid)?.reveal();
+// 生成一个聊天页面
+function openChatView(uid: number, c2c: boolean) {
+    let label: string | undefined = c2c ? Global.client.fl.get(uid)?.remark : Global.client.gl.get(uid)?.group_name;
+    if (webviewMap.has([c2c, uid])) { // 读取页面缓存
+        return webviewMap.get([c2c, uid])?.reveal();
     }
-    // generate a chat view
-    const chatView = vscode.window.createWebviewPanel("chat", label, -1, {
+    const chatView = vscode.window.createWebviewPanel("chat", label as string, -1, {
         enableScripts: true,
         enableCommandUris: true,
         retainContextWhenHidden: true
     });
-    // add chat view to map
-    webviewMap.set(uid, chatView);
-    chatView.webview.html = setHtml(uid, type, chatView.webview);
+    // 添加页面缓存
+    webviewMap.set([c2c, uid], chatView);
+    chatView.webview.html = setHtml(uid, c2c, chatView.webview);
     chatView.reveal();
     chatView.onDidDispose(() => {
-        webviewMap.delete(uid);
+        webviewMap.delete([c2c, uid]);
     });
     chatView.webview.onDidReceiveMessage(async (data: WebviewPostData) => {
         try {
             if (data.command === "getChatHistory" && data.params?.[0] === "") {
                 let buf: Buffer;
-                if (type) { // group
-                    buf = Buffer.alloc(21);
-                } else {
+                if (c2c) {
                     buf = Buffer.alloc(17);
+                } else {
+                    buf = Buffer.alloc(21);
                 }
                 buf.writeUInt32BE(uid, 0);
                 data.params[0] = buf.toString("base64");
@@ -75,21 +77,23 @@ function openChatView(uid: number, type: boolean) {
                 if (ret instanceof Map) {
                     ret = [...ret.values()];
                 }
-                // ret.echo = data.echo;
                 chatView.webview.postMessage({echo: data.echo, data: ret});
             }
         } catch { }
     });
 }
 
-function postPrivateEvent(data: oicq.PrivateMessageEvent | oicq.PrivateMessage) {
-    webviewMap.get(data.sender.user_id)?.webview.postMessage(data);
+// 向页面发送私聊信息
+function postPrivateEvent(event: oicq.PrivateMessageEvent | oicq.PrivateMessage) {
+    webviewMap.get([true, event.sender.user_id])?.webview.postMessage(event);
 }
 
-function postGroupEvent(data: oicq.GroupMessageEvent) {
-    webviewMap.get(data.group_id)?.webview.postMessage(data);
+// 向页面发送群聊信息
+function postGroupEvent(event: oicq.GroupMessageEvent) {
+    webviewMap.get([false, event.group_id])?.webview.postMessage(event);
 }
 
+// 绑定消息事件
 function bind() {
     Global.client.on("message.group", postGroupEvent);
     Global.client.on("message.private", postPrivateEvent);
