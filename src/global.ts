c51342b0da61c8c105bf3e99d0ea9e8a22cd2764
Vi-brance <vi_brance@outlook.com>
2022-12-30 21:48:05 +0800

    删减NOOP变量，添加全局类的注释说明

--- src/global.ts
+++ src/global.ts
@@ -1,12 +1,12 @@
 import * as vscode from 'vscode';
 import * as oicq from 'oicq';
 
+//全局类
 class Global {
+    // 扩展菜单
     public static context: vscode.ExtensionContext;
+    // 客户端信息
     public static client: oicq.Client;
 }
 
-// no operation
-const NOOP = () => { };
-
-export { Global, NOOP };
+export { Global };
