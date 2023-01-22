8e04fe9009229abc2560096d8e30874067c13b8d
Vi-brance <vi_brance@outlook.com>
2023-01-23 00:08:03 +0800

    侧边栏视图设为全局变量

--- src/global.ts
+++ src/global.ts
@@ -1,12 +1,14 @@
 import * as vscode from 'vscode';
 import * as oicq from 'oicq';
 
-//全局类
+/** 全局类 */
 class Global {
-    // 扩展菜单
-    public static context: vscode.ExtensionContext;
-    // 客户端信息
-    public static client: oicq.Client;
+    /** 扩展菜单 */
+    static context: vscode.ExtensionContext;
+    /** 客户端信息 */
+    static client: oicq.Client;
+    /** 侧边栏视图 */
+    static qliteTreeView: vscode.TreeView<vscode.TreeItem>;
 }
 
 export { Global };
