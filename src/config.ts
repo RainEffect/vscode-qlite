4aba1a699af011b6670e4faa1b5c3416110742f7
Vi-brance <vi_brance@outlook.com>
2023-01-23 00:12:48 +0800

    增加对登陆账号列表的记录

--- src/config.ts
+++ src/config.ts
@@ -1,31 +1,30 @@
-import * as vscode from 'vscode';
 import * as oicq from 'oicq';
 import * as fs from 'fs';
 import * as path from 'path';
 import { Global } from './global';
 
-// 配置类
+/** 配置信息类 */
 interface Config extends oicq.Config {
-    // 用户账号
-    account?: number,
-    // 密码（md5加密）
-    password?: string
+    /** 登录账号记录 */
+    accounts: Map<number, string>
+    /** 最近登陆的账号 */
+    recentLogin: number
 }
 
-// 配置文件信息
-let config: Config | undefined;
+/** 配置文件信息 */
+let config: Config;
 
-// 默认配置信息
+/** 默认配置信息 */
 const defaultConfig: Config = {
-    account: 0,
-    password: ""
+    accounts: new Map,
+    recentLogin: 0
 };
 
 /**
  * 获取配置文件地址
  * @returns 配置文件地址
  */
-function getConfigFilePath() {
+function getConfigFilePath(): string {
     return path.join(Global.context.globalStorageUri.fsPath, "config.json");
 }
 
@@ -35,36 +34,63 @@ function getConfigFilePath() {
  */
 function getConfig(): Config {
     try {
-        config = JSON.parse(fs.readFileSync(getConfigFilePath(), { encoding: "utf-8" }));
+        config = JSON.parse(fs.readFileSync(getConfigFilePath(), {
+            encoding: "utf-8"
+        }));
+        const map = new Map();
+        for (let k of Object.keys(config.accounts)) {
+            // @ts-ignore
+            map.set(Number(k), (config.accounts[k]));
+        }
+        config.accounts = map;
     } catch {
-        fs.writeFileSync(getConfigFilePath(), JSON.stringify(defaultConfig, null, 2));
+        fs.writeFileSync(
+            getConfigFilePath(),
+            JSON.stringify(defaultConfig, null, 2)
+        );
         config = defaultConfig;
+    } finally {
+        return config;
     }
-    return config as Config;
 }
 
 /**
  * 修改配置文件
- * @param conf 要修改的配置信息
+ * @param info 新增账号信息
  */
-function setConfig(conf?: Config) {
-    Object.assign(getConfig(), conf ? conf : { account: 0, password: "" });
-    fs.writeFileSync(getConfigFilePath(), JSON.stringify(config, null, 2));
+function setConfig(account?: number, password?: string) {
+    if (!account) {
+        config.recentLogin = 0;
+    } else {
+        if (!config.accounts.size) {
+            config.accounts = new Map;
+        }
+        if (password && !config.accounts.has(account)) {
+            config.accounts.set(account, password);
+        }
+        config.recentLogin = account;
+    }
+    const obj = Object.create(null);
+    for (let [k, v] of config.accounts) {
+        obj[k] = v;
+    }
+    fs.writeFileSync(getConfigFilePath(), JSON.stringify({
+        accounts: obj, recentLogin: config.recentLogin
+    }, null, 2));
 }
 
 /**
  * 返回登录配置信息
  * @returns 完整的配置信息
  */
-function genClientConfig(): Config {
-    const clientConfig: oicq.Config = {
+function genClientConfig(): oicq.Config {
+    return {
         data_dir: Global.context.globalStorageUri.fsPath,
         ignore_self: false,
         log_level: "off",
         reconn_interval: 0,
         platform: 5
     };
-    return Object.assign(clientConfig, getConfig());
 }
 
 export { getConfig, setConfig, genClientConfig };
