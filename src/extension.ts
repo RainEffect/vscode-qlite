c5d0bc77838f211c24da942a93f409ef42f05c4f
Vi-brance <vi_brance@outlook.com>
2023-01-23 00:10:15 +0800

    将注册资料显示命令移到启动扩展中

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import { Global } from './global';
 import * as client from './client';
+import * as view from './view';
 import * as chat from './chat';
 
 // 扩展启动
@@ -12,9 +13,12 @@ export function activate(context: vscode.ExtensionContext) {
 		fs.mkdirSync(Global.context.globalStorageUri.fsPath);
 	}
 	// 注册命令
-	context.subscriptions.push(vscode.commands.registerCommand("qlite.login", client.login));
+	context.subscriptions.push(vscode.commands.registerCommand("qlite.login", client.inputAccount));
+	context.subscriptions.push(vscode.commands.registerCommand("qlite.setting", client.setting));
 	context.subscriptions.push(vscode.commands.registerCommand("qlite.chat", chat.openChatView));
 	context.subscriptions.push(vscode.commands.registerCommand("qlite.search", chat.search));
+	context.subscriptions.push(vscode.commands.registerCommand("qlite.friendProfile", view.showFriendProfile));
+	context.subscriptions.push(vscode.commands.registerCommand("qlite.groupProfile", view.showGroupProfile));
 }
 
 // 扩展关闭
