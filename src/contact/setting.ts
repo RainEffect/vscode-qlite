0b59a01584fcc0513a79460b6fc8d4d1764e4b86
Vi-brance <vi_brance@outlook.com>
2023-04-21 21:41:20 +0800

    拆解client文件，登录功能移到login-view中，设置功能独立成setting文件

---
+++ src/contact/setting.ts
@@ -0,0 +1,79 @@
+import * as vscode from 'vscode';
+import Global from '../global';
+import AccountManager from '../account';
+
+/** 当前状态 */
+export let selectedStatus: number = 11;
+/** 状态选项 */
+export const statusMap: Map<number, string> = new Map([
+  [11, '在线'],
+  [60, 'Q我吧'],
+  [31, '离开'],
+  [50, '忙碌'],
+  [70, '请勿打扰'],
+  [41, '隐身']
+]);
+
+/**
+ * 设置，响应setting指令
+ */
+export function setting() {
+  /** 设置选项 */
+  const settings: vscode.QuickPickItem[] = [
+    {
+      label: '$(account) 账号管理',
+      description: `${Global.client.nickname}(${Global.client.uin})`
+    },
+    {
+      label: '$(bell) 我的状态',
+      description: statusMap.get(selectedStatus)
+    }
+  ];
+  vscode.window.showQuickPick(settings).then((value) => {
+    switch (value) {
+      case settings[0]:
+        const accounts: vscode.QuickPickItem[] = [];
+        AccountManager.getAll().forEach((uin: number) => {
+          accounts.push({ label: '$(account) ' + uin });
+        });
+        accounts.push({ label: '$(log-out) 退出' });
+        vscode.window.showQuickPick(accounts).then((account) => {
+          if (!account) {
+            return;
+          }
+          Global.client.logout();
+          if (account === accounts[accounts.length - 1]) {
+            // 退出
+            Global.loginViewProvider.setEmptyView(true);
+            vscode.commands.executeCommand(
+              'setContext',
+              'qlite.isOnline',
+              false
+            );
+          } else {
+            // 切换账号
+            const uin = Number(account.label.substring(11));
+            Global.client.login(uin);
+            AccountManager.setRecent(uin);
+          }
+        });
+        break;
+      case settings[1]:
+        const statusArray = [...statusMap.values()];
+        vscode.window
+          .showQuickPick([...statusMap.values()], {
+            placeHolder: '当前状态：' + statusMap.get(Global.client.status)
+          })
+          .then((value) => {
+            if (value === undefined) {
+              return;
+            }
+            selectedStatus = [...statusMap.keys()][statusArray.indexOf(value)];
+            if (Global.client.isOnline()) {
+              Global.client.setOnlineStatus(selectedStatus);
+            }
+          });
+        break;
+    }
+  });
+}
