1c81dd582cba3a4cfa111f7aeefbb8aaa2de34fc
Vi-brance <vi_brance@outlook.com>
2023-06-17 01:46:35 +0800

    减少搜索层级，直接同时对好友和群聊进行搜索

--- src/contact/search.ts
+++ src/contact/search.ts
@@ -1,59 +1,46 @@
-import { QuickPickItem, commands, window } from 'vscode';
+import { QuickPickItem, QuickPickItemKind, commands, window } from 'vscode';
 import Global from '../global';
 import { ChatType } from '../message/parse-msg-id';
 
-/** 搜索条目 */
-interface SearchItem extends QuickPickItem {
-  type: ChatType;
-}
-
-/**
- * 搜索栏
- * @todo 将好友和群聊以`separater`分隔
- */
+/** 搜索栏 */
 export default function search() {
-  if (!Global.client.isOnline()) {
-    return;
+  const searchList: QuickPickItem[] = [];
+  for (const friend of Global.client.fl.values()) {
+    const fItem: QuickPickItem = {
+      label: '$(person) ' + (friend.remark ? friend.remark : friend.nickname),
+      alwaysShow: false,
+      description: friend.user_id.toString()
+    };
+    searchList.push(fItem);
   }
-  const searchList: SearchItem[] = [];
-  const searchMenu = ['$(person) 搜索好友', '$(organization) 搜索群聊'];
-  window.showQuickPick(searchMenu).then((value) => {
-    if (!value) {
-      return;
-    } else if (value === searchMenu[0]) {
-      for (const friend of Global.client.fl.values()) {
-        const fItem: SearchItem = {
-          label: friend.remark ? friend.remark : friend.nickname,
-          type: ChatType.Friend
-        };
-        fItem.alwaysShow = false;
-        fItem.description = String(friend.user_id);
-        searchList.push(fItem);
+  const seperator: QuickPickItem = {
+    label: '',
+    kind: QuickPickItemKind.Separator
+  };
+  searchList.push(seperator);
+  for (const group of Global.client.gl.values()) {
+    const gItem: QuickPickItem = {
+      label: '$(organization) ' + group.group_name,
+      alwaysShow: false,
+      description: group.group_id.toString()
+    };
+    searchList.push(gItem);
+  }
+  window
+    .showQuickPick(searchList, {
+      placeHolder: '搜索好友/群聊',
+      matchOnDescription: true
+    })
+    .then((item) => {
+      if (!item) {
+        return;
       }
-    } else {
-      for (const group of Global.client.gl.values()) {
-        const gItem: SearchItem = {
-          label: group.group_name,
-          type: ChatType.Group
-        };
-        gItem.alwaysShow = false;
-        gItem.description = String(group.group_id);
-        searchList.push(gItem);
-      }
-    }
-    window
-      .showQuickPick(searchList, {
-        matchOnDescription: true,
-        placeHolder: value.split(' ')[1]
-      })
-      .then((value) => {
-        if (value) {
-          commands.executeCommand(
-            'qlite.chat',
-            Number(value.description),
-            value.type
-          );
-        }
-      });
-  });
+      commands.executeCommand(
+        'qlite.chat',
+        Number(item.description),
+        item.label.split(' ')[0].includes('person')
+          ? ChatType.Friend
+          : ChatType.Group
+      );
+    });
 }
